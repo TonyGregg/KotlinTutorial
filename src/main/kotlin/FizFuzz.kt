43f82e8f4889c4c1bdc6194b6323c644b2430b09
Antony G <antonygenil@gmail.com>
2022-10-29 16:58:59 -0500

    Added loop

---
+++ src/main/kotlin/FizFuzz.kt
@@ -0,0 +1,18 @@
+class FizFuzz {
+    fun fizFuzzFn(i: Int) = when {
+        i %  15 == 0 -> "FizBuzz"
+        i % 3 == 0 -> "Fizz"
+        i % 5 == 0 -> "Buzz"
+
+        else -> "$i" // else return the i itself
+    }
+
+
+}
+
+fun main(args: Array<String>) {
+    val fizFuzz = FizFuzz();
+    for (i in 1..100) {
+        println(fizFuzz.fizFuzzFn(i))
+    }
+}
